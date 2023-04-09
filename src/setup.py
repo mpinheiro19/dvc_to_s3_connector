82fb94d85efef9d2499b1eaaf8a74125f696843e
Marco <mpinheiro19@gmail.com>
2023-04-09 02:36:56 -0300

    :hammer: adding poc for LDA topic modeling

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
-#nltk.download('popular')
-#load_param = "en_core_web_sm"
-import spacy
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 import pandas as pd
 
 ## Setup topic modelling using LDA in scikit learn
@@ -8,6 +8,18 @@ import pandas as pd
 if __name__ == '__main__':
 
     
-    df = pd.read_csv('./data/processed/airline_occurences.csv', sep=';')
-
+    df = pd.read_csv('./data/processed/airline_occurences.csv', sep=';', index_col=0)
+    print(df.columns)
     text_col = df["report"]
+
+    n_features = 4
+    cv = CountVectorizer(max_df=0.85, min_df= 2, stop_words='english')
+    dtm = cv.fit_transform(text_col)
+
+    lda = LatentDirichletAllocation(n_components=n_features, random_state=42)
+    topic_results = lda.fit_transform(dtm)
+
+    df['topic'] = topic_results.argmax(axis=1)
+    df['cls_topic'] = cv.get_feature_names_out()[df['topic']] # trying to get major topic class needs refinement
+
+    df.to_csv("./data/processed/airline_occurences.csv", sep = ';', index=False)
